6d43494be9ce141b204b21e257b83647e6fc9803
dboyd42 <bhatmagician@gmail.com>
2019-10-18 21:23:21 -0500

    working on split line then convert to int

---
+++ avgSum.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+# Copyright 2019 David Boyd, all rights reserved
+# Program: Average Sum Calculator
+# Description:
+#     This program calculcates the sum and average of n numbers.
+# Date: Fri Oct 18 19:58:19 CDT 2019
+# Revised:
+#     <revision date>
+
+# Vars
+$average = 0;
+$size = 10;
+$sum = 0;
+
+
+# Get user input
+if (@ARGV) {
+    @numbers = @ARGV;
+} else {
+    print "Input numbers: ";
+    chomp ($nums);
+    @numbers = split (/\d+/, $nums);
+    #Requires <CR> between numbers
+    #for ($i=0; $i<$size; ++$i) {
+        #chomp ($num = <STDIN>);
+        #push @numbers, $num;
+        #}
+
+}
+$numbers_length = @numbers;
+
+calculate_sum();
+calculate_avg();
+
+# Display results
+print "Sum    : $sum \n";
+print "Average: $average \n";
+
+# Calculate the sum
+sub calculate_sum {
+    foreach $element (@numbers) {
+        $sum += $element;
+    }
+}
+# Calculate average
+sub calculate_avg {
+    $average = ($sum / $numbers_length);
+}
+
