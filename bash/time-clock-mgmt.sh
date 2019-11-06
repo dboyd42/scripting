5e8441cbf0f276d1c5c8c13de2455b3d6a4e632a
dboyd42 <bhatmagician@gmail.com>
2019-11-06 14:18:05 -0600

    basic flow successfull | modularizing

--- bash/time-clock-mgmt.sh
+++ bash/time-clock-mgmt.sh
@@ -21,13 +21,13 @@ sumMins=()        # int [beforeLuch, aferLunch]
 workedHours=0     # total hours worked
 workedMins=0      # total mins worked
 minsDaytime=""    # str "mins+daytime" (am or pm)
-daytime=""        # str "am" -or "pm"
 complement=0      # int paid clocked-in-time
 
 ###
 ### Get user input
 ###
 punchStrTimes=(6:30am 11:25am 11:40am 5:30pm)  # hardcoded to be deleted later
+#punchStrTimes=(6:10am 11:50am 11:40am 5:30pm)  # hardcoded to be deleted later
 #read -p "Time-In  : " punchStrTimes[0]        # THIS WORKS!
 #read -p "Lunch-Out: " punchStrTimes[1]
 #read -p "Lunch-In : " punchStrTimes[2]
@@ -36,60 +36,71 @@ punchStrTimes=(6:30am 11:25am 11:40am 5:30pm)  # hardcoded to be deleted later
 ###
 ### Parse Hours, Mins, Daytime
 ###
-for (( i=0; i<${#punchStrTimes[@]}; i++))
-do
-    punchedHours+=(${punchStrTimes[$i]%%:*})
-    minsDaytime=(${punchStrTimes[$i]##*:})
+parsePunchStrTimes() {
+    for (( i=0; i<${#punchStrTimes[@]}; i++))
+    do
+        # Parse using ':'
+        punchedHours+=(${punchStrTimes[$i]%%:*})
+        minsDaytime=(${punchStrTimes[$i]##*:})
 
-    # Parse Mins from am/pm
-    if [[ $minsDaytime = *"a"* ]]; then
-        punchedMins+=(${minsDaytime%%a*})
-    elif [[ $minsDaytime = *"p"* ]]; then
-        punchedMins+=(${minsDaytime%%p*})
-        punchedHours[$i]=$((punchedHours[$i] + 12))  # Convert to military time
-    else
-        punchedMins+=$((minsDaytime))
-    fi
-done
+        # Parse minsDaytime using 'a' or 'p'
+        if [[ $minsDaytime = *"a"* ]]; then
+            punchedMins+=(${minsDaytime%%a*})
+
+        elif [[ $minsDaytime = *"p"* ]]; then
+            punchedMins+=(${minsDaytime%%p*})
+
+            # Convert to military time
+            punchedHours[$i]=$((punchedHours[$i] + 12))
+
+        else
+            punchedMins+=$((minsDaytime))
+        fi
+    done
+}
 
 ###
 ### Calulate hours worked
 ###
-let sumHours+=(punchedHours[1]-punchedHours[0])
-#let sumHours+=(punchedHours[3]-punchedHours[2])
-
-###############################################################################
-echo ""
-echo "DEBUG :: sumHours: " ${sumHours[*]}
-###############################################################################
+calcHours() {
+    let sumHours+=(punchedHours[1]-punchedHours[0])
+    #let sumHours+=(punchedHours[3]-punchedHours[2])
+    ###########################################################################
+    echo ""
+    echo "DEBUG :: sumHours   : " ${sumHours[*]}
+    ###########################################################################
+}
+###############****************************************************************
+# Test Functions
+parsePunchStrTimes
+calcHours
+###############****************************************************************
 
 ###
 ### Calulate mins worked
 ###
-
-# time-in's (Minute's) complement
+# punch-In's minutes complement
 let complement=mphr-punchedMins[0]
-# Add compliment to time-out
-let tempTotMins=complement+punchedMins[1]
-#let sumMins+=tempTotMins%mphr
-
-#if [ $((tempTotMins-mphr)) -gt 0 ]; then
-#    let workedHours+=tempTotMins-60
-#fi
+# Add compliment to punch-Out's minutes
+let sumMins+=complement+punchedMins[1]
 ###############################################################################
-echo ""
-echo "DEBUG :: sumMins: " ${tempTotMins[*]}
+echo "DEBUG :: sumMins    : " ${sumMins[*]}
 ###############################################################################
 
 ###
 ### Convert ints to mins
 ###
 
-#if [[ totalMin -lt mphr ]]
-#then
-#    sum
-#
-
+if [[ $sumMins -lt $mphr ]]
+then
+    let sumHours[0]--
+else
+    let sumMins[0]=sumMins[0]%mphr
+fi
+###############################################################################
+echo "DEBUG :: sumHours   : " ${sumHours[*]}
+echo "DEBUG :: sumMins    : " ${sumMins[*]}
+###############################################################################
 
 
 
@@ -103,7 +114,9 @@ echo "punchedHours : " ${punchedHours[*]}
 echo "punchedMins  : " ${punchedMins[*]}
 echo ""
 echo "complement   : " $complement
-echo "tempTotMins  : " $tempTotMins
+echo ""
+echo "sumHours     : " ${sumHours[*]}
+echo "sumMins      : " ${sumMins[*]}
 echo "workedMins   : " $workedMins
 echo "workedHours  : " $workedHours
 echo ""
