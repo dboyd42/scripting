fde5ddc0848219693a0c4328816c34c3c5653b46
dboyd42 <bhatmagician@gmail.com>
2019-11-07 21:31:51 -0600

    updated

--- bash/sqr-numbers.sh
+++ bash/sqr-numbers.sh
@@ -13,25 +13,46 @@
 ###
 array=()    # original array
 sqrArr=()   # modified array
-inFile="./data-files/arr01.txt"
+#inFile="./data-files/arr01.txt"
+inFile="/dev/null"
 tempFile="./temp.txt"
 outFile="./outFile_sqr-N"
+boolFileExists=0
+
+###
+### Check for file existence
+###
+fileExists() {
+    # check if file is exists
+    if [ -f "$1" ]; then
+        boolFileExists=1
+        inFile=$1
+        echo "if -f \$1"
+    elif [ -r $inFile ]; then
+        boolFileExists=1
+        echo "elif -f \$inFile"
+    else
+        read -p "Enter filename: " $inFile
+        #clear
+        #data-files/arr01.txt
+        fileExists $inFile
+
+    fi
+}
 
 ###
 ### Read File Into an Array
 ###
-if [[ -f "$inFile" ]]; then
-    # Read file
-    while IFS='/' read -r line; do
-        # strip '/' from file
-        line=${line///}
-        # Read $line into array
-        read -r -a array <<< $line
-    done < $inFile
-else
-    # ask for new file
-    echo 'Invalid file'
-fi
+readFile() {
+    if [[ $boolFileExists -eq 1 ]]; then
+        while IFS='/' read -r line; do
+            # strip '/' from file
+            line=${line///}
+            # Read $line into array
+            read -r -a array <<< $line
+        done < $inFile
+    fi
+}
 
 ###
 ### Calculate & Write Squared Values to File
@@ -41,4 +62,7 @@ do
     echo $(($i ** 2)) >> $outFile
     sqrArr+=$(($i ** 2))
 done
+
+fileExists "$inFile"
+echo $boolFileExists
 
