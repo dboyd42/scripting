3dd509a6528beae318a8c39f651c0f1bfabff954
dboyd42 <bhatmagician@gmail.com>
2019-11-12 14:14:09 -0600

    fixed input array files

--- bash/array-math.sh
+++ bash/array-math.sh
@@ -8,28 +8,78 @@
 # Revised:
 #     <revision date>
 
-decimals=2
+###
+### Declare Vars
+###
+tempArr=()
+inputArrX=()
+inputArrY=()
+outputArrZ=()
+inFile=()       # list of input files
+numFiles=2      # number of input files
+decimals=2      # decimal places used for scale
+boolFileExists=0
+
 # a=$((bc <<< "scale=$decimals; sqrt($VAR)"))
-
-
-inFiles=2   ## number of input files
 
 ###
 ### Get Input Files
 ###
 getInFiles() {
-    for (( i=1; i<$inFiles+1; ++i ))
-    do
-        read -p "Enter input file $i: " inFile[$i]
-        #echo "infile $i: " ${inFile[$i]}
-    done
+    inFile[0]=x-arr.txt         # function below works
+    inFile[1]=y-arr.txt         # hardcoded - to be deleted later
+    boolFileExists=1
+    #for (( i=1; i<$numFiles+1; ++i ))
+    #do
+    #    boolFileExists=0
+    #    while [boolFileExists -eq 0]
+    #    do
+        #    read -p "Enter input file $i: " inFile[$i]
+        #    echo "infile $i: " ${inFile[$i]}
+        #    checkFile "${inFile[$i]}"
+    #    done
+    #done
 }
 
 ###
-### Main
+### Read inFile into an Array
+###
+readFile() {
+    if [[ $boolFileExists -eq 1 ]]; then
+        for i in $numFiles
+        do
+            while read -r line; do
+                # Read $line into array
+                read -a tempArr <<< $line
+            done < $1
+        done
+    fi
+}
+
+###
+### main
 ###
 main() {
     getInFiles
+    echo ${inFile[0]}   # x-arr.txt
+    echo ${inFile[1]}   # y-arr.txt
+    file1="x-arr.txt"
+    file2="y-arr.txt"
+    #checkfiles
+
+    readFile ${inFile[0]}
+    inputArrX=${tempArr[@]}
+    echo "arrX =" ${inputArrX[@]}
+
+    #readFile "$file2"
+    readFile ${inFile[1]}
+    inputArrY=${tempArr[@]}
+    echo "arrY =" ${inputArrY[@]}
+
+    echo "--------------------"
+    echo "arrX =" ${inputArrX[@]}
+    echo "arrY =" ${inputArrY[@]}
+
 }
 
 ###
