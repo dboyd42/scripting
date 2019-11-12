9ece4b59f90f83ce714d630fd0085fe6d7800ea1
dboyd42 <bhatmagician@gmail.com>
2019-11-12 14:54:20 -0600

    completed multiplyArrays, sqrRootArray

--- bash/array-math.sh
+++ bash/array-math.sh
@@ -11,74 +11,100 @@
 ###
 ### Declare Vars
 ###
-tempArr=()
-inputArrX=()
-inputArrY=()
-outputArrZ=()
-inFile=()       # list of input files
-numFiles=2      # number of input files
-decimals=2      # decimal places used for scale
-boolFileExists=0
+boolFileExists=0  # value for inFile's existence
+decimals=2        # decimal places used for scale
+numFiles=2        # number of input files
+tempArr=()        # holds the temp input arrays
+inFile=()         # list of input files
+inputArrX=()      # first array
+inputArrY=()      # second array
+multiArr=()       # multiplied arrays value
+outputArrZ=()     # square-root values
 
-# a=$((bc <<< "scale=$decimals; sqrt($VAR)"))
+
+# used for hardcoding while testing
+    inFile[0]=x-arr.txt         # function below works
+    inFile[1]=y-arr.txt         # hardcoded - to be deleted later
+    boolFileExists=1
+
 
 ###
 ### Get Input Files
 ###
 getInFiles() {
-    inFile[0]=x-arr.txt         # function below works
-    inFile[1]=y-arr.txt         # hardcoded - to be deleted later
-    boolFileExists=1
-    #for (( i=1; i<$numFiles+1; ++i ))
-    #do
-    #    boolFileExists=0
-    #    while [boolFileExists -eq 0]
-    #    do
-        #    read -p "Enter input file $i: " inFile[$i]
-        #    echo "infile $i: " ${inFile[$i]}
-        #    checkFile "${inFile[$i]}"
-    #    done
-    #done
+    for (( i=1; i<$numFiles+1; ++i ))
+    do
+        boolFileExists=0
+        while [boolFileExists -eq 0]
+        do
+           read -p "Enter input file $i: " inFile[$i]
+           echo "infile $i: " ${inFile[$i]}
+           checkFile "${inFile[$i]}"
+        done
+    done
+}
+
+###
+### Multiply arrays
+###
+###
+multiplyArrays() {
+    #findGreatestSize
+    size=${#inputArrX[@]}
+    for (( i=0; i<$size; ++i ))
+    do
+        multiArr[$i]=$(( ${inputArrX[$i]} * ${inputArrY[$i]} ))
+    done
+    echo "multiArr's : " ${multiArr[@]}
 }
 
 ###
 ### Read inFile into an Array
 ###
-readFile() {
-    if [[ $boolFileExists -eq 1 ]]; then
-        for i in $numFiles
-        do
-            while read -r line; do
-                # Read $line into array
-                read -a tempArr <<< $line
-            done < $1
-        done
-    fi
+#readFile() {
+
+#}
+
+###
+### Square Root Array
+###
+sqrRoot() {
+    echo
+    #for i in "${multiArr[@]}"
+    for (( i=0; i<$size; ++i ))
+    do
+        outputArrZ[$i]=$( bc <<< "scale=$decimals; sqrt(${multiArr[$i]})")
+                  #avg=$( bc <<< "scale=$decimals;$sum/$numbers_length")
+    done
+    echo "output z's : " ${outputArrZ[@]}
 }
 
 ###
 ### main
 ###
 main() {
-    getInFiles
+    # get input filenames
+    #getInFiles
     echo ${inFile[0]}   # x-arr.txt
     echo ${inFile[1]}   # y-arr.txt
-    file1="x-arr.txt"
-    file2="y-arr.txt"
     #checkfiles
 
-    readFile ${inFile[0]}
-    inputArrX=${tempArr[@]}
-    echo "arrX =" ${inputArrX[@]}
+    # Get input arrays                                  ### stderr: != array
+    readarray -d ' ' inputArrX < ${inFile[0]}
+    readarray -d ' ' inputArrY < ${inFile[1]}
 
-    #readFile "$file2"
-    readFile ${inFile[1]}
-    inputArrY=${tempArr[@]}
-    echo "arrY =" ${inputArrY[@]}
+    # multiple arrays
+    multiplyArrays
 
+    # square root multiplied array
+    sqrRoot
+
+    # square root multipled array
+
+    # display output
     echo "--------------------"
-    echo "arrX =" ${inputArrX[@]}
-    echo "arrY =" ${inputArrY[@]}
+    echo "arrX =" ${inputArrX[@]} "| size = " ${#inputArrX[@]}
+    echo "arrY =" ${inputArrY[@]} "| size = " ${#inputArrY[@]}
 
 }
 
