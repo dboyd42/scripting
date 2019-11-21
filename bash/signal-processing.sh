5e40c76f09a4df79a5bda51ccd16a31254e14ff1
dboyd42 <bhatmagician@gmail.com>
2019-11-21 17:12:40 -0600

    initial commit

--- bash/signal-processing.sh
+++ bash/signal-processing.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 # Copyright 2019 David Boyd, all rights reserved
-# Program: <program name>
+# Program: Signal Processing
 # Description:
-#     <what the program does>
-# Date: <today's date>
+#
+# Date: Thu Nov 21 2019
 # Revised:
-#     <revision date>
+#   <revision date>
+# Reference:
+#   http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_12_02.html
 
+clear
+# trap interrupt
+trap '' 1 2 3 9 15 20
 
+Age=88
+#clear
 
+echo -n "Please enter Grandma's age: "
+read Guess
+
+while [[ $Guess -ne $Age ]]
+do
+    echo "Wrong guess"
+    echo "Please try again"
+    echo "Please enter Grandma's age: "
+    read Guess
+done
+
+echo "Congratulations!"
+echo "user gets Candy"
 
