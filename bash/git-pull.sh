ed1056d8567dbfbb8f056354568b5a41bf4d3331
dboyd42 <bhatmagician@gmail.com>
2019-12-29 17:31:48 -0600

    added git status

--- bash/git-pull.sh
+++ bash/git-pull.sh
@@ -2,22 +2,27 @@
 # Copyright 2019 David Boyd, all rights reserved
 # Program: git-pull.sh
 # Description:
-#     Updates all repositories within a given directory.
+#     Updates (pulls) and checks the status of repos
+#     under a parent directory.
 # Date: Dec 29 2019
 # Revised:
 #     <revision date>
 
 #read -p "Enter path/to/dir that holds your repos [enter '.' for current]: " repo
 #cd $repo
-echo -e "Current directory: $PWD\n"
+cd ../../
+echo -e "\nCurrent directory: $PWD\n"
 
 # update each repo
 for dir in ./*/
 do
-    cd $dir && echo "$dir"
+    cd $dir
+    echo "$dir"
     git pull
+    git status
+    echo -e "-----------------------------------------------\n"
     cd ../
 done
 
-echo -e "Completed pulling.\n"
+echo -e "\nCompleted pulling.\n"
 
