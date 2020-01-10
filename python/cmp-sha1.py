e8337cb4965d880e52136684b915da98afad5b52
dboyd42 <bhatmagician@gmail.com>
2020-01-10 02:58:30 -0600

    added termcolor for results

--- python/cmp-sha1.py
+++ python/cmp-sha1.py
@@ -9,6 +9,7 @@
 import sys          # used for CLI args
 import subprocess   # used for terminal commands
 import pyperclip    # cp & paste from sys clipboard
+from termcolor import colored
 
 # Display warning
 #print("Make sure the SHA1 checksum is copied into the your system's clipboard")
@@ -32,18 +33,16 @@ output = output.decode('utf-8')      # type = str
 # Extract SHA1sum
 sha1 = output.split()[0]
 
-# Compare SHA1sums
-if (web_sha1 == sha1):
-    status = "Passed"
-else:
-    status = "Failed!"
-
 # Display Results
 print(web_sha1, "\t<--From System Clipboard")
 print(sha1, "\t<--From file's SHA1")
 
 print('='*79)
-print('SHA1 checksums:', status)
-
-
+# Compare SHA1sums
+if (web_sha1 == sha1):
+    status = "Passed"
+    print('SHA1 checksums:', colored(status, 'green'))
+else:
+    status = "Failed"
+    print('SHA1 checksums:', colored(status, 'red'))
 
