936c24038720e4ae7678b0be6d397fdd55408ccd
dboyd42 <bhatmagician@gmail.com>
2020-01-18 22:07:26 -0600

    fix md5 errs | formatted output

--- python/cmp-sums.py
+++ python/cmp-sums.py
@@ -28,17 +28,17 @@ local_file = sys.argv[1]
 web_sha1 = pyperclip.paste()
 web_md5 = pyperclip.paste()
 
-# Set up the sha1sum cmd and direct the output to a pipe
+# Run checksums cmd and store it in a class
 file_sha1 = subprocess.Popen(['sha1sum', local_file], stdout=subprocess.PIPE)
 file_md5 = subprocess.Popen(['md5sum', local_file], stdout=subprocess.PIPE)
 
-# Run the command
+# Exctract bytes then convert the cmd outputs to strings
 outSHA1 = file_sha1.communicate()[0]  # type = bytes
-outMD5 = file_sha1.communicate()[0]  # type = bytes
-outSHA1 = outSHA1.decode('utf-8')      # type = str
-outMD5 = outMD5.decode('utf-8')      # type = str
+outMD5 = file_md5.communicate()[0]    # type = bytes
+outSHA1 = outSHA1.decode('utf-8')     # type = str
+outMD5 = outMD5.decode('utf-8')       # type = str
 
-# Extract SHA1sum
+# Extract checksums
 sha1 = outSHA1.split()[0]
 md5 = outMD5.split()[0]
 
@@ -46,8 +46,13 @@ md5 = outMD5.split()[0]
 if (web_sha1 == sha1):
     sha1Status = "Passed"
     sha1Color = "green"
+    md5Status = "Failed"
+    md5Color = "red"
+elif (web_md5 == md5):
     md5Status = "Passed"
     md5Color = "green"
+    sha1Status = "Failed"
+    sha1Color = "red"
 else:
     sha1Status = "Failed"
     sha1Color = "red"
@@ -55,10 +60,13 @@ else:
     md5Color = "red"
 
 # Display Results
-print(web_sha1, "\t<--From System Clipboard")
-print(sha1, "\t<--From file's SHA1")
 print('='*79)
-print('SHA1 checksums:', sha1Colored(sha1Status, sha1Color))
-print('MD5 checksums:', md5Colored(md5Status, md5Color))
-
+print(web_sha1, "\t<--From System Clipboard")
+print(sha1, "\t<--From file's SHA1\n")
+print(web_sha1, "\t<--From System Clipboard")
+print(md5, "\t<--From file's MD5")
+print('='*79)
+print('[',sys.argv[1],']')
+print('SHA1 checksums:', colored(sha1Status, sha1Color))
+print('MD5  checksums:', colored(md5Status, md5Color))
 
